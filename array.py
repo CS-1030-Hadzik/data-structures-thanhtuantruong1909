ae218cea38e53899f7a86bab1047301872b33c04
Thanh Truong <W01375365@cs.weber.edu>
2020-04-05 21:56:46 -0600

    finished

---
+++ array.py
@@ -0,0 +1,16 @@
+print('Thanh Tuan Truong')
+cars=['Ford', 'Chrysler','Dodge','Ram','Jeep','Chevy','GMC']
+print (cars)
+print (len(cars))
+cars = cars + ['Buick']
+print (cars)
+print (cars[3])
+cars[2]='Toyota'
+print (cars)
+cars.pop(4)
+print (cars)
+cars.sort()
+print (cars)
+my_array_lenth = len(cars)
+my_string = 'The length of my array is'
+print (my_string, my_array_lenth)
